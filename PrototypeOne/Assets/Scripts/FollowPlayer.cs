8c1b9d29af2f8d03efede47cbd624dd48e1e58ec
Kaique da Silva <kaeyosthaeron@gmail.com>
2022-08-04 12:55:23 -0300

    feat: add late update to smooth the camera

--- PrototypeOne/Assets/Scripts/FollowPlayer.cs
+++ PrototypeOne/Assets/Scripts/FollowPlayer.cs
@@ -14,7 +14,7 @@ public class FollowPlayer : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         // Offset the camera based on player's position
         transform.position = player.transform.position + offset;
